550cd5dc3b09136833941b74db530e497f5d780e
sojung <thwjd0564@naver.com>
2022-09-08 16:28:49 +0900

    우편주소찾기 api

--- join.php
+++ join.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="./join.css">
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
   </head>
   <body>
     <!-- 헤더 영역 -->
@@ -75,7 +76,7 @@
             <th>주소<img src="img/ico_required_blue.gif" alt=""></th>
             <td>
               <div>
-                <input type="text" class="zipCode_Text" name="" value=""><span class="zipCode_Search">우편번호</span>
+                <input type="text" class="zipCode_Text" id="mem_postcode"><span class="zipCode_Search" onclick="PostcodeSearch();">우편번호</span>
               </div>
               <div>
                 <input type="text" class="adress_defualt" id="mem_adress1" value="">기본주소
@@ -277,9 +278,55 @@ function member_join(){
   });
 
 }
+//
+// function idCheck(){}
+//
+// function passCheck(){}
 
-function idCheck(){}
+function PostcodeSearch(){
+      new daum.Postcode({
+          oncomplete: function(data) {
+              // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
-function passCheck(){}
+              // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+              // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+              var addr = ''; // 주소 변수
+              var extraAddr = ''; // 참고항목 변수
+
+              //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+              if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                  addr = data.roadAddress;
+              } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                  addr = data.jibunAddress;
+              }
+
+              // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+              if(data.userSelectedType === 'R'){
+                  // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                  // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                  if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                      extraAddr += data.bname;
+                  }
+                  // 건물명이 있고, 공동주택일 경우 추가한다.
+                  if(data.buildingName !== '' && data.apartment === 'Y'){
+                      extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                  }
+                  // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                 if(extraAddr !== ''){
+                     extraAddr = ' (' + extraAddr + ')';
+                 }
+                 // 조합된 참고항목을 기본주소 뒤에 추가한다.
+                  addr += extraAddr;
+             }
+
+              // 우편번호와 주소 정보를 해당 필드에 넣는다.
+              document.getElementById('mem_postcode').value = data.zonecode;
+              document.getElementById("mem_adress1").value = addr;
+              // 커서를 상세주소 필드로 이동한다.
+              document.getElementById("mem_adress2").focus();
+          }
+      }).open();
+  }
+
 
 </script>
